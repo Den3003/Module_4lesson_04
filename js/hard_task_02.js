16f2f97575e57afb0f1ecd0449253a2d8f61ef33
Denis <denisbormusov@gmail.com>
2024-05-15 18:02:50 +0500

    module_4lesson_04

--- js/hard_task_02.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-{
-  const calcMinNumber = (a, b) => a < b ? a : b;
-  const resultCalcMinNumber = calcMinNumber(10, 9);
-  console.log(`Минимальное число ${resultCalcMinNumber}`);
-}
